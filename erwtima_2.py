4d0a43895757cb24db9807a86f6549dca9f1957f
p19kavv1 <72515382+p19kavv1@users.noreply.github.com>
2020-11-23 19:51:27 +0200

    Update erwtima_2.py

--- erwtima_2.py
+++ erwtima_2.py
@@ -1,5 +1,7 @@
 import math
 
-print('The entropy of the Greek alphabet equally likely to each letter = ',math.log(num_letters,2)
+h_equal = math.log(num_letters,2)
+
+print('The entropy of the Greek alphabet equally likely to each letter = ',h_equal)
       
       
